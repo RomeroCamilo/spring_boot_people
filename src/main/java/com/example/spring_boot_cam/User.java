7afd65981dc16314a1d447dfbb4d0cfea841f5cc
Camilo Romero <romerocamilo24@gmail.com>
2024-08-07 12:08:46 -0400

    add user

--- src/main/java/com/example/spring_boot_cam/User.java
+++ src/main/java/com/example/spring_boot_cam/User.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List; // Import this
 
-
+/* specificy which collection in our database we are using */
 @Document(collection = "users")
 public class User {
 
